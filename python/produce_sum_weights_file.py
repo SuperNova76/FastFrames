db1c8a4502137e765a4abd535efdae7d0bbf6b31
Michal Dubovsky <michal.dubovsky@cern.ch>
2023-10-24 17:27:05 +0200

    Adding 2 python scripts for producing filelist and metadata text files

---
+++ python/produce_sum_weights_file.py
@@ -0,0 +1,57 @@
+from ROOT import TFile, TTree
+from sys import argv
+import os
+
+class Metadata:
+    def __init__(self):
+        self.dsid        = 0
+        self.campaign    = ""
+        self.data_type = "mc"
+
+    def get_metadata_tuple(self):
+        return (self.dsid, self.campaign, self.data_type)
+
+def get_list_of_root_files_in_folder(folder_path : str) -> list:
+    return [os.path.join(folder_path, file) for file in os.listdir(folder_path) if file.endswith(".root")]
+
+def get_metadata_string(root_file : TFile, key : str) -> str:
+    tnamed_object = root_file.Get(key)
+    if tnamed_object == None:
+        raise Exception("Could not find key {} in file {}".format(key, root_file.GetName()))
+    return tnamed_object.GetTitle()
+
+def get_file_metadata(file_path : str) -> Metadata:
+    metadata = Metadata()
+    root_file = TFile(file_path)
+    metadata.dsid        = int(get_metadata_string(root_file, "dsid"))
+    metadata.campaign    = get_metadata_string(root_file, "campaign")
+    metadata.data_type = get_metadata_string(root_file, "dataType")
+    root_file.Close()
+    return metadata
+
+def produce_filelist(root_files_folder : str) -> None:
+    sample_map = {}
+
+    root_files = get_list_of_root_files_in_folder(root_files_folder)
+    filelist_address = root_files_folder + "/filelist.txt"
+    for root_file in root_files:
+        metadata = get_file_metadata(root_file)
+        metadata_tuple = metadata.get_metadata_tuple()
+        if metadata_tuple not in sample_map:
+            sample_map[metadata_tuple] = []
+        sample_map[metadata_tuple].append(root_file)
+
+    MAX_METADATA_ITEM_LENGTHS = [8 for i in range(len(metadata_tuple))]
+    with open(filelist_address, "w") as filelist:
+        for metadata_tuple, root_files in sample_map.items():
+            for root_file in root_files:
+                for metadata_element in metadata_tuple:
+                    n_spaces = MAX_METADATA_ITEM_LENGTHS[metadata_tuple.index(metadata_element)] - len(str(metadata_element))
+                    filelist.write(str(metadata_element) + n_spaces*" ")
+                filelist.write("{}\n".format(root_file))
+
+
+if __name__ == "__main__":
+    if len(argv) != 2:
+        raise Exception("Usage: python produce_filelist.py <folder_path>")
+    produce_filelist(argv[1])
