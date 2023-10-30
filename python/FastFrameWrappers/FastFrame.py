310fa15b21628a810a6729c9f5347c3f70e3a071
Michal Dubovsky <michal.dubovsky@cern.ch>
2023-10-30 13:18:01 +0100

    Python interface for FastFrame

--- python/FastFrameWrappers/FastFrame.py
+++ python/FastFrameWrappers/FastFrame.py
@@ -10,10 +10,21 @@ set_paths()
 
 from ConfigReaderCpp import ConfigReaderCppGeneral, MainFrame
 from python_wrapper.python.logger import Logger
-
+from ConfigReader import ConfigReader
 
 
 if __name__ == "__main__":
+
+    config_reader = None
+    if len(sys.argv) > 1:
+        config_file_path = sys.argv[1]
+        config_reader = ConfigReader(config_file_path)
+        Logger.set_log_level(config_reader.block_general.debug_level)
+        Logger.log_message("Config file path: " + config_file_path)
+
+
     main_frame = MainFrame()
+    if config_reader is not None:
+        main_frame.setConfigReader(config_reader.getPtr())
     main_frame.init()
     main_frame.executeHistograms()
