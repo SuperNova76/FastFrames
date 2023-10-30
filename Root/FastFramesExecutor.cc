5b297450ed1b7d0649bcb24e7af1caf2e647b403
Tomas Dado <tomas.dado@cern.ch>
2023-10-30 13:38:29 +0100

    Added main wrapper class

---
+++ Root/FastFramesExecutor.cc
@@ -0,0 +1,54 @@
+#include "FastFrames/FastFramesExecutor.h"
+
+#include "FastFrames/ConfigSetting.h"
+#include "FastFrames/Logger.h"
+#include "FastFrames/MainFrame.h"
+
+#include "ROOT/RDataFrame.hxx" // for implicit MT
+#include "TClass.h"
+#include "TSystem.h"
+
+#include <exception>
+#include <string>
+
+FastFramesExecutor::FastFramesExecutor(const std::shared_ptr<ConfigSetting>& config) :
+m_config(config)
+{
+}
+
+void FastFramesExecutor::runFastFrames() const {
+    ROOT::EnableImplicitMT(m_config->numCPU());
+
+    TClass* c(nullptr);
+    std::unique_ptr<MainFrame> baseFrame(nullptr);
+
+    //const std::string name = "CustomFrame";
+    const std::string name = "";
+
+    if (!name.empty()) {
+      // read the custom code for adding variables
+      const std::string libName = "lib" + name + ".so";
+      gSystem->Load(libName.c_str());
+
+      c = ::TClass::GetClass(name.c_str());
+      if (!c) {
+        LOG(ERROR) << "Cannot get class " << name << "\n";
+        throw std::invalid_argument("");
+      }
+      baseFrame.reset(static_cast<MainFrame*>(c->New()));
+      if (!baseFrame) {
+        LOG(ERROR) << "Something went wrong with creating the custom class\n";
+        throw std::invalid_argument("");
+      }
+    } else {
+      baseFrame = std::make_unique<MainFrame>();
+    }
+
+    baseFrame->setConfig(m_config);
+
+    baseFrame->init();
+
+    baseFrame->executeHistograms();
+
+    LOG(INFO) << "Finished running the code\n";
+}
