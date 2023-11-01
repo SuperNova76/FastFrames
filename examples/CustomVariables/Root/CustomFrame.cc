a1cb2bb56b682ab8055299f93b378efaf4448a6d
Tomas Dado <tomas.dado@cern.ch>
2023-11-01 22:49:36 +0100

    Finally a working version with a custom define

--- examples/CustomVariables/Root/CustomFrame.cc
+++ examples/CustomVariables/Root/CustomFrame.cc
@@ -1,6 +1,5 @@
 #include "CustomFrame/CustomFrame.h"
 
-#include "FastFrames/DefineHelpers.h"
 #include "FastFrames/UniqueSampleID.h"
 
 
@@ -14,9 +13,9 @@ ROOT::RDF::RNode CustomFrame::defineVariables(ROOT::RDF::RNode mainNode,
   //   id.simulation() return simulation flavour
   // You can use it in your functions to apply only per sample define
 
-  auto LeadingJetPt = [](const std::vector<ROOT::Math::PtEtaPhiEVector>& jets,
+  auto LeadingJetPt = [this](const std::vector<ROOT::Math::PtEtaPhiEVector>& jets,
                          const std::vector<char>& passed) {
-    auto sortedJets = DefineHelpers::sortedPassedVector(jets, passed);
+    auto sortedJets = this->sortedPassedVector(jets, passed);
     return sortedJets.at(0).pt();    
   };
 
@@ -53,7 +52,7 @@ bool CustomFrame::passes4Jets50GeV1Btag(const std::vector<ROOT::Math::PtEtaPhiEV
 
   // first get the ordered indices of the selected jets (that passed the current selection)
 
-  std::vector<std::size_t> sortedIndices = DefineHelpers::sortedPassedIndices(fourVec, selected);
+  std::vector<std::size_t> sortedIndices = this->sortedPassedIndices(fourVec, selected);
 
   // now get the ordered vectors for four momentum and btagging
   std::vector<ROOT::Math::PtEtaPhiEVector> sortedTLV = this->vectorFromIndices(fourVec, sortedIndices);
@@ -70,4 +69,114 @@ bool CustomFrame::passes4Jets50GeV1Btag(const std::vector<ROOT::Math::PtEtaPhiEV
   }
 
   return (nJets >= 4) && (nTags > 0);
+}
+
+
+
+/// --------------------------------------------------------------------------
+/// Other helper methods that can be used
+/// --------------------------------------------------------------------------
+std::vector<TLV> CustomFrame::sortedPassedVector(const std::vector<TLV>& tlv,
+                                                 const std::vector<char>& passedSelection,
+                                                 const std::vector<char>& passedOR) const {
+
+  // do some checks first
+  const std::size_t size = tlv.size();
+  if (size != passedSelection.size() || size != passedOR.size()) {
+    LOG(ERROR) << "Sizes of the vectors do not match!\n";
+    throw std::invalid_argument("");
+  }
+
+  // select TLVs
+  std::vector<TLV> result;
+  for (std::size_t i = 0; i < size; ++i) {
+    if (!passedSelection.at(i)) continue;
+    if (!passedOR.at(i)) continue;
+
+    result.emplace_back(tlv.at(i));
+  }
+
+  // sort them based on pT
+  std::sort(result.begin(), result.end(), [](const TLV& v1, const TLV& v2){return v1.pt() > v2.pt();});
+
+  return result;
+}
+
+std::vector<TLV> CustomFrame::sortedPassedVector(const std::vector<TLV>& tlv,
+                                                 const std::vector<char>& passedSelection) const {
+
+  // do some checks first
+  const std::size_t size = tlv.size();
+  if (size != passedSelection.size()) {
+    LOG(ERROR) << "Sizes of the vectors do not match!\n";
+    throw std::invalid_argument("");
+  }
+
+  // select TLVs
+  std::vector<TLV> result;
+  for (std::size_t i = 0; i < size; ++i) {
+    if (!passedSelection.at(i)) continue;
+
+    result.emplace_back(tlv.at(i));
+  }
+
+  // sort them based on pT
+  std::sort(result.begin(), result.end(), [](const TLV& v1, const TLV& v2){return v1.pt() > v2.pt();});
+
+  return result;
+}
+
+std::vector<std::size_t> CustomFrame::sortedPassedIndices(const std::vector<TLV>& tlv,
+                                                          const std::vector<char>& selection) const {
+
+
+  // some checkes first
+  const std::size_t size = tlv.size();
+  if (size != selection.size()) {
+    LOG(ERROR) << "Sizes of the vectors do not match!\n";
+    throw std::invalid_argument("");
+  }
+
+  std::vector<std::pair<double, std::size_t> > forSorting;
+
+  for (std::size_t i = 0; i < size; ++i) {
+    if (!selection.at(i)) continue;
+    forSorting.emplace_back(std::make_pair(tlv.at(i).pt(), i));
+  }
+
+  std::sort(forSorting.begin(), forSorting.end(), [](const auto& element1, const auto& element2){return element1.first > element2.first;});
+
+  std::vector<std::size_t> result;
+  for (const auto& ielement : forSorting) {
+    result.emplace_back(ielement.second);
+  }
+  return result;
+}
+
+std::vector<std::size_t> CustomFrame::sortedPassedIndices(const std::vector<TLV>& tlv,
+                                                          const std::vector<char>& selection1,
+                                                          const std::vector<char>& selection2) const {
+
+  // some checkes first
+  const std::size_t size = tlv.size();
+  if (size != selection1.size() || size != selection2.size()) {
+    LOG(ERROR) << "Sizes of the vectors do not match!\n";
+    throw std::invalid_argument("");
+  }
+
+  std::vector<std::pair<double, std::size_t> > forSorting;
+
+  for (std::size_t i = 0; i < size; ++i) {
+    if (!selection1.at(i)) continue;
+    if (!selection2.at(i)) continue;
+    forSorting.emplace_back(std::make_pair(tlv.at(i).pt(), i));
+  }
+
+  std::sort(forSorting.begin(), forSorting.end(), [](const auto& element1, const auto& element2){return element1.first > element2.first;});
+
+  std::vector<std::size_t> result;
+  for (const auto& ielement : forSorting) {
+    result.emplace_back(ielement.second);
+  }
+  return result;
 }
