c108b97101e9cc2fec998e5e55f096c88658af3f
Michal Dubovsky <michal.dubovsky@cern.ch>
2023-11-07 16:22:35 +0100

    Support for command line options

--- python/FastFrames.py
+++ python/FastFrames.py
@@ -2,9 +2,9 @@
 The main script for running the whole framework from python.
 It takes the path to the config file as the first argument.
 """
-
 import os
 import sys
+import argparse
 
 this_dir = "/".join(os.path.dirname(os.path.abspath(__file__)).split("/")[0:-1])
 sys.path.append(this_dir)
@@ -15,16 +15,33 @@ set_paths()
 from ConfigReaderCpp import ConfigSettingWrapper, FastFramesExecutor
 from python_wrapper.python.logger import Logger
 from ConfigReader import ConfigReader
+from CommandLineOptions import CommandLineOptions
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
 
-    if len(sys.argv) < 2:
-        raise ValueError("Please specify the path to the config file as the first argument")
+    parser.add_argument("--config",  help="Path to the config file")
+    parser.add_argument("--step",    help="Step to run: 'n' (ntuples) or 'h' (histograms). Default: 'h'", nargs = '?',  default="h")
+    parser.add_argument("--samples", help="Comma separated list of samples to run. Default: all",                       default="all")
+    args = parser.parse_args()
 
-    config_file_path = sys.argv[1]
-    Logger.log_message("INFO", "Going to read the config: " + config_file_path)
-    config_reader = ConfigReader(config_file_path)
+    config_path = args.config
+    step        = args.step
+    if step != "h" and step != "n":
+        Logger.log_message("ERROR", "Unknown step: {}".format(step))
+
+    samples     = args.samples
+    if samples == "all":
+        samples = None
+    else:
+        samples = samples.split(",")
+
+    command_line_options = CommandLineOptions()
+    command_line_options.set_samples(samples)
+
+    Logger.log_message("INFO", "Going to read the config: " + config_path)
+    config_reader = ConfigReader(config_path)
     Logger.set_log_level(config_reader.block_general.debug_level)
 
     fast_frames_executor = FastFramesExecutor(config_reader.block_general.cpp_class.getPtr())
